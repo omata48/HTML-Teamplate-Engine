f10f186ad3e462220254fc12e8b917772ad8af21
Omar <62365988+omata48@users.noreply.github.com>
2020-07-14 09:04:32 -0500

    app creating file in output correctly

--- app.js
+++ app.js
@@ -9,13 +9,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
-
 
 async function getTeamData() {
 
@@ -82,6 +78,8 @@ async function getTeamData() {
     ]);
     engineeringTeam.push(new Manager(name, 1, email, officeNumber));
     
+    // for teamsize given, make employee objects
+    console.log("\nNow asking some questions about the rest of the team:\n")
     for (var i = 0; i < teamSize; i++){
 
         // assign iteration variables to use with new obj
@@ -96,9 +94,8 @@ async function getTeamData() {
         } else {
             console.log("error with adding member");
         }
+        console.log('\n');
     }
-
-    // add manager obj to list 
 
     // console.log(engineeringTeam);
     return engineeringTeam;
@@ -108,16 +105,22 @@ async function getTeamData() {
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-getTeamData().then(function(teamArray){
-    console.log(teamArray);
+getTeamData().then(async function(teamArray){
+    // console.log(teamArray);
+    const htmlBlock = await render(teamArray)
+    // console.log(htmlBlock);
+
+    // After you have your html, you're now ready to create an HTML file using the HTML
+    // returned from the `render` function. Now write it to a file named `team.html` in the
+    // `output` folder. You can use the variable `outputPath` above target this location.
+    // Hint: you may need to check if the `output` folder exists and create it if it
+    // does not.
+
+    fs.writeFileSync(outputPath,htmlBlock,"utf8")
+    
 })
 
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
